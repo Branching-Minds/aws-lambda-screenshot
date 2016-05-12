275a3f510e7dfcabff626bc9d1f2ffbed08c9c7c
wefarrell <we.farrell@gmail.com>
2016-05-11 23:53:42 -0400

    renamed exports

---
+++ exports.js
@@ -0,0 +1,34 @@
+var webshot = require('webshot');
+var AWS = require('aws-sdk');
+var Readable = require('stream').Readable;
+var s3 = new AWS.S3();
+
+// stream seems to be empty -> phantomjs not working on Lambda?
+var options = {screenSize: { width: 1024, height: 768 }, streamType: 'jpeg'}; 
+
+exports.handler = function(event, context) {
+  webshot(event.url, options, function(err, stream){
+    if (err) {
+      context.fail(err);
+    } else {
+
+      var key;
+      
+      if (event.key !== undefined) {
+        key = event.key + '.jpeg';
+      } else {
+        key = event.url + '.jpeg';
+      }
+
+      var body = new Readable().wrap(stream);
+      var params = {Bucket: event.bucket, Key: key, Body: body, ACL: 'public-read'};
+      s3.upload(params, function(err, data) {
+        if (err) {
+          context.fail(err);
+        } else {
+          context.succeed(data);
+        }
+      });
+    }
+  });
+};
